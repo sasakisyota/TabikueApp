fd4805a01c995d17dabce441a1dbe4939641373a
syota <sasakisyota@icloud.com>
2021-08-20 12:12:37 +0900

    Initial Commit

---
+++ TabikueApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabikueApp
+//
+//  Created by 佐々木翔太 on 2021/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabikueApp/TabikueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabikueAppApp.swift
+//  TabikueApp
+//
+//  Created by 佐々木翔太 on 2021/08/20.
+//
+
+import SwiftUI
+
+@main
+struct TabikueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
